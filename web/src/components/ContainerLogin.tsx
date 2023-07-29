0835e0303fb0c2460ae77078f42a1c9661d52923
MarceloKade <98852912+MarceloKade@users.noreply.github.com>
2023-07-28 23:59:32 -0300

    feat: update ContainerLogin

---
+++ web/src/components/ContainerLogin.tsx
@@ -0,0 +1,14 @@
+export default function ContainerLogin() {
+    return (
+        <>
+            <div>
+                <form action="">
+                    <label htmlFor="">Login</label>
+                    <input type="text" />
+                    <label htmlFor="">senha</label>
+                    <input type="text" />
+                </form>
+            </div>
+        </>
+    );
+}
